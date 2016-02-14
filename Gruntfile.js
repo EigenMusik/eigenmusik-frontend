8343d71d941292c92c2bd8c8eecbc1cc4c436ed3
Tim Coulson <tim@justpark.com>
2016-02-14 12:50:04 +0000

    Include eigenmusik-messages repository in the build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -392,6 +392,12 @@ module.exports = function (grunt) {
           cwd: 'bower_components/bootstrap/dist',
           src: 'fonts/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          src: ['**'],
+          cwd: 'eigenmusik-messages/',
+          dest: '<%= yeoman.dist %>/',
+          filter: 'isFile'
         }]
       },
       styles: {
